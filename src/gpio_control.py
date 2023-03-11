22b6a7fb3dd601e7f5f8dd610f5725d6f082a00a
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-03-11 11:01:05 -0800

    Update gpio_control.py

--- src/gpio_control.py
+++ src/gpio_control.py
@@ -16,27 +16,30 @@ from std_msgs.msg import Bool
 def toggleGPIO(gui_data):
   
   # Set GPIO output for each pin equal to the value passed from the subscriber
-  GPIO.output(7, gui_data.gpio_pin_7)
-  GPIO.output(11, gui_data.gpio_pin_11)
-  GPIO.output(13, gui_data.gpio_pin_13)
-  GPIO.output(15, gui_data.gpio_pin_15)
+  GPIO.output(23, gui_data.gpio_pin_7)
+  GPIO.output(24, gui_data.gpio_pin_11)
+  GPIO.output(25, gui_data.gpio_pin_13)
+  GPIO.output(5, gui_data.gpio_pin_15)
   GPIO.output(19, gui_data.gpio_pin_19)
+  GPIO.output(16, gui_data.gpio_pin_16)
 # Main function
 if __name__ == "__main__":
   GPIO.setmode(GPIO.BOARD)
   GPIO.setwarnings(False)
   
   # Initialize GPIOs in output mode
-  GPIO.setup(7, GPIO.OUT)
-  GPIO.output(7, 0)
-  GPIO.setup(11, GPIO.OUT)
-  GPIO.output(11, 0)
-  GPIO.setup(13, GPIO.OUT)
-  GPIO.output(13, 0)
-  GPIO.setup(15, GPIO.OUT)
-  GPIO.output(15, 0)
+  GPIO.setup(23, GPIO.OUT)
+  GPIO.output(23, 0)
+  GPIO.setup(24, GPIO.OUT)
+  GPIO.output(24, 0)
+  GPIO.setup(25, GPIO.OUT)
+  GPIO.output(25, 0)
+  GPIO.setup(5, GPIO.OUT)
+  GPIO.output(5, 0)
   GPIO.setup(19, GPIO.OUT)
   GPIO.output(19, 0)
+  GPIO.setup(16, GPIO.OUT)
+  GPIO.output(16, 0)
   
   # Create ROS Node and Subscriber
   rospy.init_node("gpio_control")
